6dca4e166f79ed7819c92be8671dc2f85558483c
Elijah <ElijahFrankle626@gmail.com>
2023-03-11 12:13:31 -0800

    Add Music Manager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    void Start()
+    {
+        //Called when the scene is loaded
+    }
+
+    void Update()
+    {
+        //Called every frame
+    }
+}
